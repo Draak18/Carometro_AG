390b54db605854e273e4a9527b3626b49ad66076
Alexandre Deboletta <40008747806@escola928.edu>
2024-04-16 15:23:48 -0300

    RouterBase

---
+++ Carometro Alê/src/pages/home/Home.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import "./Home.css";
+
+function Home() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <p1>macaco</p1>
+      </div>
+    </>
+  );
+}
+
+export default Home;
