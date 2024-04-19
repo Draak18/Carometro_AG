eac351329196633ecfa32a6838cded2e5f8d90b5
Alexandre Deboletta <40008747806@escola928.edu>
2024-04-19 09:27:17 -0300

    19/04 comentado

--- Carometro Alê/src/components/header/Header.jsx
+++ Carometro Alê/src/components/header/Header.jsx
@@ -1,10 +1,15 @@
+// Link é utilizado para criar links de navegação entre diferentes rotas da aplicação
 import { Link } from "react-router-dom";
+
+// Import de CSS
 import "./Header.css";
 import Logo from "./../../images/logoSenai.jfif";
+
 function Header() {
   return (
     <header>
       <div>
+        {/* Cada `Link` representa um link para uma rota específica na aplicação */}
         <Link to="/">Home</Link>
         <Link to="/turmas">Turmas</Link>
         <Link to="/alunos">Alunos</Link>
--- Carometro Alê/src/routes.jsx
+++ Carometro Alê/src/routes.jsx
@@ -1,4 +1,8 @@
+// Import Routes é o componente que encapsula as rotas da aplicação
+// Import Route é utilizado para definir uma rota específica a seu componente associado
 import { Routes, Route } from "react-router-dom";
+
+// Importa os componentes para os caminhos das rotas
 import Home from "./pages/home/Home";
 import Turmas from "./pages/turma/Turmas";
 import Login from "./pages/login/Login";
@@ -6,9 +10,12 @@ import Alunos from "./pages/alunos/Alunos";
 import Foto from "./pages/foto/Foto";
 import Cadastro from "./pages/cadastro/Cadastro";
 
+// Define o componente MainRoutes
 function MainRoutes() {
   return (
+    // Bloco de rotas da aplicação utilizando o componente Routes
     <Routes>
+      {/* Define uma rota para o caminhos definidos e associa-os a um componente */}
       <Route path="/" element={<Home />} />
       <Route path="/turmas" element={<Turmas />} />
       <Route path="/login" element={<Login />} />
