fdd3b9685d33a286396974a70473c88c0ee16439
Alexandre Deboletta <40008747806@escola928.edu>
2024-04-29 16:39:34 -0300

    juntoPesquisapronta

--- Carômetro Junto/src/pages/alunos/Alunos.jsx
+++ Carômetro Junto/src/pages/alunos/Alunos.jsx
@@ -1,8 +1,7 @@
 import "./Alunos.css";
 import Header2 from "../../components/header/Header2.jsx";
-import lupaImg from "../../images/lupa.png";
-import Rosto from "../../images/rosto.png"
-
+import PesquisaComponente from "../../components/pesquisaComponente/PesquisaComponente.jsx";
+import Gradiente from "../../images/gradiente.png";
 
 function Alunos() {
   return (
@@ -12,41 +11,80 @@ function Alunos() {
         <div className="tituloAlunos">
           <h1>IDEV2</h1>
         </div>
-        <div className="pesquisa">
-            <input type="text" />
-            <button id="consultarBotao" type="submit" className="consultarBotao">
-              <img className="lupaImg" src={lupaImg} />
-            </button>
-        </div>
+        <PesquisaComponente />
       </div>
       <div className="rostoAlunos">
-        
         <div className="aluno">
-          <h1>iiiiiiiii</h1>
+          <div className="sombra">
+            <img src={Gradiente} />
+          </div>
+          <div className="linha"></div>
+          <div className="textos">
+            <div className="nomeAluno">A</div>
+          </div>
         </div>
         <div className="aluno">
-          <h1>iiiiiiiii</h1>
+          <div className="sombra">
+            <img src={Gradiente} />
+          </div>
+          <div className="linha"></div>
+          <div className="textos">
+            <div className="nomeAluno">A</div>
+          </div>
         </div>
         <div className="aluno">
-          <h1>iiiiiiiii</h1>
+          <div className="sombra">
+            <img src={Gradiente} />
+          </div>
+          <div className="linha"></div>
+          <div className="textos">
+            <div className="nomeAluno">A</div>
+          </div>
         </div>
         <div className="aluno">
-          <h1>iiiiiiiii</h1>
+          <div className="sombra">
+            <img src={Gradiente} />
+          </div>
+          <div className="linha"></div>
+          <div className="textos">
+            <div className="nomeAluno">A</div>
+          </div>
         </div>
         <div className="aluno">
-          <h1>iiiiiiiii</h1>
+          <div className="sombra">
+            <img src={Gradiente} />
+          </div>
+          <div className="linha"></div>
+          <div className="textos">
+            <div className="nomeAluno">A</div>
+          </div>
         </div>
         <div className="aluno">
-          <h1>iiiiiiiii</h1>
+          <div className="sombra">
+            <img src={Gradiente} />
+          </div>
+          <div className="linha"></div>
+          <div className="textos">
+            <div className="nomeAluno">A</div>
+          </div>
         </div>
         <div className="aluno">
-          <h1>iiiiiiiii</h1>
+          <div className="sombra">
+            <img src={Gradiente} />
+          </div>
+          <div className="linha"></div>
+          <div className="textos">
+            <div className="nomeAluno">A</div>
+          </div>
         </div>
         <div className="aluno">
-          <h1>iiiiiiiii</h1>
-        </div>
-        <div className="aluno">
-          <h1>iiiiiiiii</h1>
+          <div className="sombra">
+            <img src={Gradiente} />
+          </div>
+          <div className="linha"></div>
+          <div className="textos">
+            <div className="nomeAluno">A</div>
+          </div>
         </div>
       </div>
     </div>
@@ -54,7 +92,3 @@ function Alunos() {
 }
 
 export default Alunos;
-
-
-
-
