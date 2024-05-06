211aec2c43f62482aa1003364e30d0745c8db4f3
Gabrielle Quinquio <54570965806@escola928.edu>
2024-05-06 16:37:14 -0300

    alunos pronto

--- Carômetro Junto/src/components/alunocomp/AlunosComp.jsx
+++ Carômetro Junto/src/components/alunocomp/AlunosComp.jsx
@@ -1,27 +1,24 @@
 import Header from "../../components/header/Header.jsx";
 import "../alunocomp/AlunosComp.css";
 import Rosto from "../../images/rosto.png";
-import Gradiente from "../../images/gradiente.jpeg";
+import Gradiente from "../../images/gradiente.png";
 
 
 function AlunosComp() {
   return (
     <div>
-      <Header />
       <div className="aluno">
         <div className="fotoDoAluno">
-          <img className="fotR" src={Rosto} />
+          <img className="fotoR" src={Rosto} />
         </div>
         <div className="gradienteFoto">
           <img className="fotoG" src={Gradiente}  />
-
         </div>
         <div className="informacoesDoAluno">
-          {/* <h1>Raquel</h1>
-          <h2>IDEV2</h2> */}
+          <div className="nome"><h1>Alexandre Magalhães Deboletta</h1></div>
+          <div className="turma"><h3>IDEV2</h3></div>
         </div>
         <div className="linhaVermelha">
-
         </div>
       </div>
     </div>
