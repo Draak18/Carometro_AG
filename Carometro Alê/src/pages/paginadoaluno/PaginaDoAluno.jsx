7ab838243cd36238c62350ba1c8a1dca847b962c
Alexandre Deboletta <40008747806@escola928.edu>
2024-05-27 14:26:47 -0300

    27/05 tudoResponsivo

--- Carometro Alê/src/pages/paginadoaluno/PaginaDoAluno.jsx
+++ Carometro Alê/src/pages/paginadoaluno/PaginaDoAluno.jsx
@@ -1,5 +1,6 @@
 import Header from "../../components/header/Header.jsx";
 import "./PaginaDoAluno.css";
+import RostoA from "../../images/rosto.png";
 
 function PaginaDoAluno() {
   return (
@@ -7,22 +8,57 @@ function PaginaDoAluno() {
       <Header />
       <div className="fundoDoAluno">
         <div className="separarPagina">
-          <div className="fotoPaginaAluno"></div>
-          <div className="caixaInformacao"></div>
-            <div className="informacoesPaginaAluno">
-              <p>Nome</p>
-              <p>Email</p>
-              <p>CPF</p>
-              <p>Celular</p>
-              <p>Logradouro</p>
-              <p>Bairro</p>
-              <p>Cidade</p>
-              <p>Estado</p>
-              <p>CEP</p>
-              <p>Nome do responsável</p>
-              <p>Ocorrencias</p>
+          <div className="rostoAluno">
+            <img src={RostoA} />
+          </div>
+          <div className="informacoesPaginaAluno">
+            <div class="nome">
+              <h1>Gabrielle de Lima Quinquio</h1>
+              <div className="separarInformacao">
+                <div className="parte1">
+                  <div className="robson">
+                  <label>Email:</label>
+                  <input className="inputNormal" type="text" />
+                  </div>
+                  <div className="robson">
+                  <label>CPF:</label>
+                  <input className="inputNormal" type="text" />
+                  </div>
+                  <div className="robson">
+                  <label>Celular:</label>
+                  <input className="inputNormal" type="text" />
+                  </div>
+                  <div className="robson">
+                  <label>CEP:</label>
+                  <input className="inputNormal" type="text" />
+                  </div>
+                  <div className="robson">
+                  <label>Data de nascimento:</label>
+                  <input className="inputDiferente" type="text" />
+                  </div>  
+                </div>
+                <div className="parte2">
+                    <div>
+                      <label>Bairro:</label>
+                      <input className="inputNormal" type="text" />
+                    </div>
+                    <div>
+                      <label>Estado:</label>
+                      <input className="inputNormal" type="text" />
+                    </div>
+                    <div>
+                      <label>Cidade:</label>
+                      <input className="inputNormal" type="text" />
+                    </div>
+                    <div>
+                      <label>Rua:</label>
+                      <input className="inputNormal" type="text" />
+                    </div>
+                </div>
+              </div>
             </div>
           </div>
+        </div>
       </div>
     </div>
   );
