f6639399cdfbc42e5b80add58136bf9f90a33b66
Draak18 <darkmerman07@gmail.com>
2024-05-28 00:56:46 -0300

    28/05 madrugada

--- Carômetro Junto/src/pages/paginadoaluno/PaginaDoAluno.jsx
+++ Carômetro Junto/src/pages/paginadoaluno/PaginaDoAluno.jsx
@@ -1,10 +1,16 @@
 import Header from "../../components/header/Header.jsx";
 import "./PaginaDoAluno.css";
 import RostoA from "../../images/rosto.png";
+import React, { useState, useEffect } from "react";
 
 function PaginaDoAluno() {
+  const [showCard, setShowCard] = useState(false);
+
+  const mostrarCard = () => {
+    setShowCard(!showCard);
+  };
   return (
-    <div >
+    <div>
       <Header />
       <div className="fundoDoAluno">
         <div className="separarPagina">
@@ -13,56 +19,79 @@ function PaginaDoAluno() {
           </div>
           <div className="informacoesPaginaAluno">
             <div class="tudo">
-            <div className="nome">
-              <h1>Gabrielle de Lima Quinquio</h1>
-            </div>
+              <div className="nome">
+                <h1>Gabrielle de Lima Quinquio</h1>
+              </div>
               <div className="separarInformacao">
                 <div className="parte1">
                   <div className="espacamento">
-                  <label id="teste">Email:</label>
-                  <input className="inputNormal" type="text" />
+                    <label id="teste">Email:</label>
+                    <input className="inputNormal" type="text" />
                   </div>
                   <div className="espacamento">
-                  <label id="teste">CPF:</label>
-                  <input className="inputNormal" type="text" />
+                    <label id="teste">CPF:</label>
+                    <input className="inputNormal" type="number" />
                   </div>
                   <div className="espacamento">
-                  <label id="teste">Celular:</label>
-                  <input className="inputNormal" type="text" />
+                    <label id="teste">Celular:</label>
+                    <input className="inputNormal" type="text" />
                   </div>
                   <div className="espacamento">
-                  <label id="teste">CEP:</label>
-                  <input className="inputNormal" type="text" />
+                    <label id="teste">CEP:</label>
+                    <input className="inputNormal" type="number" />
                   </div>
                   <div className="espacamento">
-                  <label id="teste">Data de Nascimento:</label>
-                  <input className="inputNormal" type="text" />
-                  </div>  
+                    <label id="teste">Data de Nascimento:</label>
+                    <input
+                      className="inputNormal"
+                      id="dataNascimento"
+                      type="date"
+                    />
+                  </div>
                 </div>
                 <div className="parte2">
-                    <div className="espacamento">
-                      <label id="teste">Bairro:</label>
-                      <input className="inputNormal" type="text" />
-                    </div>
-                    <div className="espacamento">
-                      <label id="teste">Estado:</label>
-                      <input className="inputNormal" type="text" />
-                    </div>
-                    <div className="espacamento">
-                      <label id="teste">Cidade:</label>
-                      <input className="inputNormal" type="text" />
-                    </div>
-                    <div className="espacamento">
-                      <label id="teste">Rua:</label>
-                      <input className="inputNormal" type="text" />
-                    </div>
-                    <div className="botao"></div>
+                  <div className="espacamento">
+                    <label id="teste">Bairro:</label>
+                    <input className="inputNormal" type="text" />
+                  </div>
+                  <div className="espacamento">
+                    <label id="teste">Estado:</label>
+                    <input className="inputNormal" type="text" />
+                  </div>
+                  <div className="espacamento">
+                    <label id="teste">Cidade:</label>
+                    <input className="inputNormal" type="text" />
+                  </div>
+                  <div className="espacamento">
+                    <label id="teste">Rua:</label>
+                    <input className="inputNormal" type="text" />
+                  </div>
+                  <div className="botaoPaginaAluno">
+                    <button onClick={mostrarCard}>OCORRÊNCIA</button>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
+      {showCard && (
+        <div>
+          <div className="overlay" />
+          <div className="card">
+            {
+              <div className="boxCard">
+                <button
+                  className="botaoFechar"
+                  onClick={() => setShowCard(false)}
+                >
+                  FECHAR
+                </button>
+              </div>
+            }
+          </div>
+        </div>
+      )}
     </div>
   );
 }
